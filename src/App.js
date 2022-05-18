0045a4d153502e9ecb58e041125f6421f3eaec21
Pierce <ppuckette@gmail.com>
2022-05-18 13:47:49 -0400

    Routes 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React from "react";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
 import "./App.css";
+import Checkout from "./components/Checkout";
 import Header from "./components/Header";
 import Home from "./components/Home";
 
-
 const App = () => {
-  return (
+	return (
 		<div className="App">
-      <Header />
-      <Home />
+			<Routes>
+				<Route exact path="/checkout" element={<Checkout />}>
+					<Header />
+					<Checkout />
+        </Route>
+        
+        <Route exact path="/" element={<Home />}>
+          <Header />
+          <Home />
+        </Route>
+			</Routes>
+
+			
 		</div>
 	);
-}
+};
 
 export default App;
---
+++ src/components/Checkout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../css/Checkout.css";
+
+
+const Checkout = () => {
+    return (
+        <div>
+            <h1> Hello World</h1>
+        </div>
+    );
+
+};
+
+export default Checkout;
