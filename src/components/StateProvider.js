99e9627c43a0039c89ca1b39f6bacbb2863ff7e2
Pierce <ppuckette@gmail.com>
2022-05-26 17:06:01 -0400

    missing return and curly brackets

--- src/components/StateProvider.js
+++ src/components/StateProvider.js
@@ -9,11 +9,12 @@ export const StateContext = createContext();
 
 //wrap our app and provide the data layer
 
-export const StateProvider = ({ reducer, initialState, children }) => (
-	<StateContext.Provider value={useReducer(reducer, initialState)}>
-		{children}
-	</StateContext.Provider>
-);
+export const StateProvider = ({ reducer, initialState, children }) => {
+	return (
+		<StateContext.Provider value={useReducer(reducer, initialState)}>
+			{children}
+		</StateContext.Provider>)
+};
 
 //pull information from the data layer
 
